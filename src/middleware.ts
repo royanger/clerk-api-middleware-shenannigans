a4051bb979f781f47a69d3f912c4ffcc1596c738
Artur Bodera <abodera@gmail.com>
2023-09-08 10:53:06 +1000

    Remove confusing comment.

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,6 @@ export const config = {
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
-     * - api (API routes)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
